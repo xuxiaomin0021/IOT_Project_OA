68961c7a14fdbab1ca76ec82580ce79850634bd2
2501023011 <2501023011@qq.com>
2020-06-16 16:03:21 +0800

    崔杰

---
+++ IOT_Project_OA.Model/Base_WaiQin.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IOT_Project_OA.Model
+{
+    public class Base_WaiQin
+    {
+        public Guid ID { get; set; }
+        public Guid Emp_ID { get; set; }
+        public Guid Customer_ID { get; set; }
+        public string QianDate { get; set; }
+        public string QianAddress { get; set; }
+        public string QianDistince { get; set; }
+    }
+}

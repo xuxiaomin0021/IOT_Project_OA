e9bde130ae60b32e1e0c37eb6f7153f430d98e1f
LENOVO <LENOVO@DESKTOP-8GLD169>
2020-06-16 16:52:02 +0800

    xxm

--- IOT_Project_OA.DAL/IDAL/AssetsIDAL/FilesIDAL.cs
+++ IOT_Project_OA.DAL/IDAL/AssetsIDAL/FilesIDAL.cs
@@ -9,10 +9,23 @@ namespace IOT_Project_OA.DAL.IDAL.AssetsIDAL
     public interface FilesIDAL
     {
         /// <summary>
+        /// 显示存储过程分页
+        /// </summary>
+        /// <param name="tableName">要分页的表名</param>
+        /// <param name="orderField">排序的字段</param>
+        /// <param name="whereStr">查询条件</param>
+        /// <param name="pageIndex">页码</param>
+        /// <returns></returns>
+        public ProcDataAndTotal<Base_Assete_Files> ProcPageData(string tableName,string orderField,string whereStr,int pageIndex);
+
+
+        /// <summary>
         /// 删除资产档案
         /// </summary>
         /// <param name="files">资产档案model存取ID</param>
         /// <returns></returns>
         public int DeleteFiles(Base_Assete_Files files);
+
+        
     }
 }

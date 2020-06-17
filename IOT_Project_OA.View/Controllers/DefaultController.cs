b8de0ebcf64f8563a183b6af84d49d57dc5dea6f
晋 王 <wangjin19952018@outlook.com>
2020-06-17 08:06:14 +0800

    王晋

---
+++ IOT_Project_OA.View/Controllers/DefaultController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IOT_Project_OA.View.Controllers
+{
+    public class DefaultController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult MyDeskTop()
+        {
+            return View();
+        }
+        
+    }
+}
--- IOT_Project_OA.View/Startup.cs
+++ IOT_Project_OA.View/Startup.cs
@@ -46,7 +46,7 @@ namespace IOT_Project_OA.View
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=Default}/{action=Index}/{id?}");
             });
         }
     }
